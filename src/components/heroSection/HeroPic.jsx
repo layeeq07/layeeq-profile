d806588b333df0d5152190610fe542417e53aa5f
layeeq07 <layeeqahmed07@gmail.com>
2025-05-08 22:14:17 +0530

    update paths

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../images/HexaMe.png"
+        src="../../public/images/HexaMe.png"
         alt="Mohammed Layeeq Ahmed"
         className="max-h-[450px] w-auto"
       />
