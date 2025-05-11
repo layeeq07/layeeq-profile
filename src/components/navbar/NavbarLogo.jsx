fb0cdace509efdac0f74e8046cf744bfbb32e18c
layeeq07 <layeeqahmed07@gmail.com>
2025-05-11 12:50:19 +0530

    update navbar2

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
         Mohammed Layeeq Ahmed
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        MLA
+        LA
       </h1>
     </div>
   );
